ca1372a32b9a16811d8b69c78de5339fcfe11216
AlaynaCarlin <alaynacarlin18@gmail.com>
2022-01-27 12:48:35 -0500

    major comments in validate jwt

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,19 +5,25 @@ let validateJWT = require("../middleware/validate-jwt");
 
 router.post('/post', validateJWT, async (req, res) => {
     const{product, brand, content} = req.body.post;
-    // const {id} = req.user;
-    const postEntry = {
-        product,
-        brand,
-        content
-    }
 
     try {
-        const newPost = await models.PostsModel.create(postEntry)
-       res.status(200).json(newPost)
+        await models.PostsModel.create({
+            product: product,
+            brand: brand,
+            content: content,
+            userId: req.user.id
+        })
+        .then(
+            post => {
+                res.status(201).json({
+                    post: post,
+                    message: 'successfully posted'
+                });
+            }
+        )
     } catch (err) {
         res.status(500).json({
-            error: `Failed to create post: ${err}`
+            error: `failed to create post: ${err}`
         });
     };
 });
@@ -26,3 +32,21 @@ router.post('/post', validateJWT, async (req, res) => {
 
 
 module.exports = router;
+
+
+//     const{product, brand, content} = req.body.post;
+//     // const {id} = req.user;
+//     const postEntry = {
+//         product,
+//         brand,
+//         content
+//     }
+
+//     try {
+//         const newPost = await models.PostsModel.create(postEntry)
+//        res.status(200).json(newPost)
+//     } catch (err) {
+//         res.status(500).json({
+//             error: `Failed to create post: ${err}`
+//         });
+//     };
