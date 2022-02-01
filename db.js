3b4716e6d36098a86782470953ec93d8aa250da5
AlaynaCarlin <alaynacarlin18@gmail.com>
2022-02-01 10:40:46 -0500

    fixing things

--- db.js
+++ db.js
@@ -3,7 +3,12 @@ const Sequelize = require('sequelize');
 // ! use database url
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 module.exports = sequelize;
